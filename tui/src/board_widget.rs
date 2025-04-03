bc4790d96edb153daefe688c45547c24000c0c83
Sebastian Messmer <messmer@cryfs.org>
2025-04-02 22:21:03 -0700

    Better board graphics

--- tui/src/board_widget.rs
+++ tui/src/board_widget.rs
@@ -3,7 +3,8 @@ use ratatui::{
     buffer::Buffer,
     layout::{Constraint, Rect},
     style::Stylize,
-    widgets::{Row, Table, Widget},
+    text::{Line, Text},
+    widgets::{Paragraph, Row, Table, Widget},
 };
 
 pub struct BoardWidget<'a, const BoardSize: usize>
@@ -19,28 +20,39 @@ where
     [(); bitvec::mem::elts::<usize>(2 * BoardSize * BoardSize)]:,
 {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let rows = (0..BoardSize).map(|y| {
-            Row::new(
-                (0..BoardSize)
-                    .map(|x| {
-                        let is_current_pos = self.current_pos == (x, y);
-                        let cell = self.board[(x, y)];
-                        let cell_str = match cell {
-                            Some(go_game::Player::White) => "⬤ ",
-                            Some(go_game::Player::Black) => "◯ ",
-                            None => "  ",
-                        };
-                        if is_current_pos {
-                            cell_str.on_blue().bold()
-                        } else {
-                            cell_str.into()
-                        }
-                    })
-                    .collect::<Vec<_>>(),
-            )
-        });
-        let widths = [Constraint::Length(2); BoardSize];
-        let table = Table::new(rows, widths);
-        table.render(area, buf);
+        let text = (0..BoardSize)
+            .map(|y| {
+                Line::from(
+                    (0..BoardSize)
+                        .map(|x| {
+                            let is_current_pos = self.current_pos == (x, y);
+                            let cell = self.board[(x, y)];
+                            let cell_str = match cell {
+                                Some(go_game::Player::White) => "○ ", // white stone
+                                Some(go_game::Player::Black) => "● ", // black stone
+                                None => match (x, y) {
+                                    (0, 0) => "┌─",                                             // top left corner
+                                    (0, n) if n == BoardSize - 1 => "└─", // bottom left corner
+                                    (n, 0) if n == BoardSize - 1 => "┐ ", // top right corner
+                                    (n, m) if n == BoardSize - 1 && m == BoardSize - 1 => "┘ ", // bottom right corner
+                                    (0, _) => "├─",                       // left edge
+                                    (_, 0) => "┬─",                       // top edge
+                                    (n, _) if n == BoardSize - 1 => "┤ ", // right edge
+                                    (_, n) if n == BoardSize - 1 => "┴─", // bottom edge
+                                    (_, _) => "┼─",                       // middle cell
+                                },
+                            };
+                            if is_current_pos {
+                                // TODO Only highlight the first character
+                                cell_str.on_blue().bold()
+                            } else {
+                                cell_str.into()
+                            }
+                        })
+                        .collect::<Vec<_>>(),
+                )
+            })
+            .collect::<Vec<_>>();
+        Paragraph::new(Text::from(text)).render(area, buf);
     }
 }
